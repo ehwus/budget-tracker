310a56baa1e3ff0452298df3d1c0624a28160757
Alex Withington-Smith <alex@awithsmith.com>
2021-03-23 11:30:21 +0000

    initialized project

---
+++ frontend/build/precache-manifest.97ca18a2596f1205ebe7c3b2c8b89cad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "447fea40cd4782bd0b95d3108b1f5ed8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68fad1d5e6f6c86f025c",
+    "url": "/static/css/main.e5549477.chunk.css"
+  },
+  {
+    "revision": "8b9d18f4af8461fe9403",
+    "url": "/static/js/2.919cc4e8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.919cc4e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68fad1d5e6f6c86f025c",
+    "url": "/static/js/main.26984186.chunk.js"
+  },
+  {
+    "revision": "4792b66c7ea268d3b997",
+    "url": "/static/js/runtime-main.d43be887.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
